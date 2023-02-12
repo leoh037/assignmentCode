50bd87db6664f3dff4c02239b2ca9c165c918158
Leo <hernleoj@gmail.com>
2023-02-12 06:23:15 -0500

    first come first serve done

--- scheduler/tempCodeRunnerFile.c
+++ scheduler/tempCodeRunnerFile.c
@@ -1,2 +1,25 @@
 
-                stateid = 0;
+
+                if(current->processState == 0){
+                    (current->blockTimer) = (current->blockTimer) - 1;
+                    if(current -> blockTimer == 0){
+                        //set to ready state
+                        current->processState = 1;
+                        current->runTimer = current->burstTime;
+                    }
+                }
+                else if(current->processState == 2){
+                    current->remainingTime = current->remainingTime - 1;
+                    current->runTimer = current -> runTimer - 1;
+                    if(current -> runTimer == 0){
+                        //set process to blocked stated
+                        current->processState = 0;
+                        dequeue(&head);
+                        current->queuedState = 0;
+                        current->blockTimer = current->process->io_time;
+                    }
+                    cpuCycles++;
+                }
+                if(current->remainingTime == 0){
+                    current->terminationState = 1;
+                    current->turnaroundTime = currentCycle - current->process->arrival_time + 1;
