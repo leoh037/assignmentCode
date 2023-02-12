058709cd1592392bffc00ee3afe3790bbd7591e0
Leo <hernleoj@gmail.com>
2023-02-11 20:58:08 -0500

    stuff

--- scheduler/dummy.c
+++ scheduler/dummy.c
@@ -6,14 +6,22 @@
 
 int main(){
 
-    int numberOfProcesses = 3;
-    int values[3][4] = {
+    // int numberOfProcesses = 3;
+    // int values[3][4] = {
+    //     {0, 4, 2, 0},
+    //     {1, 6, 1, 2},
+    //     {2, 8, 4, 3}
+    // };
+
+    int numberOfProcesses = 2;
+    int values[2][4] = {
         {0, 4, 2, 0},
-        {1, 6, 1, 2},
-        {2, 8, 4, 3}
+        {1, 6, 1, 2}
     };
 
     ///////////////////////////////////////
+
+    printf("running dummy...")
 
     struct node **structs = malloc(numberOfProcesses * sizeof(struct node*));   //will allocate a buffer big enough to hold n pointers struct nodes
 
@@ -31,6 +39,11 @@ int main(){
         newNode -> processState = 0;
         newNode -> terminationState = 0;
         newNode -> arrivalState = 0;
+        newNode -> queuedState = 0;
+        newNode -> burstTime = (int)(process -> cpu_time * 0.5);
+        newNode -> remainingTime = process -> cpu_time;
+        newNode -> runTimer = 0;
+        newNode -> blockTimer = newNode -> burstTime;
         newNode -> next = NULL;
         *(structs + i) = newNode;
     }
@@ -40,55 +53,75 @@ int main(){
 
     char *states[] = {"blocked", "ready", "running"};
     int timeToProcess = structs[0]->process->cpu_time;
-    int burtsTime =  (int)(structs[0]->process->cpu_time * 0.5);
+    int burtsTime;
     int currentCycle = 0;
 
-    int runtimer = burtsTime;
-    int blocktimer;
-    int stateid = 2; // 0 blocked, 2 running
     char state[10];
     strcpy(state, states[2]);
 
-    char *cycleResults[3];
+    char *cycleResults[2];
 
     struct node* current;
     int numberOfReadyProcesses = 0;
     int terminatedProcesses = 0;
+    int waitingProcesses = 0;
 
     while(terminatedProcesses < numberOfProcesses){
-
+    
+        //go through the pointer to struct node array and examine on the state of all processes
         for(int i = 0; i < numberOfProcesses; i++){
             current = structs[i];
             if (current -> process -> arrival_time == currentCycle){
                 current -> processState = 1;
                 current -> arrivalState = 1;
+                waitingProcesses++;
             }
-            if(current -> processState == 1){
+            if((current -> processState == 1) && (current -> queuedState == 0)){
                 enqueue(&head, &tail, current);
+                current -> queuedState = 1;
             }
             if(current -> terminationState == 1){
                 terminatedProcesses++;
+                waitingProcesses--;
             }
         }
-
-        sprintf(cycleResults[structs[0]->process->pid], "%d:%s", structs[0]->process->pid, states[stateid]);
-
-        if(stateid == 0){
-            blocktimer--;
-            if(blocktimer == 0){
-                stateid = 2;
-                runtimer = burtsTime;
+        //ensure that only the process at the head of the queue is in a running state, all other processes in the queue are ready, and those not on the queue are either blocked or have terminated
+        if(getSize(&head) > 0){
+            if(head -> processState != 2){
+                head -> processState = 2;
             }
         }
-        else if(stateid == 2){
-            timeToProcess--;
-            runtimer--;
-            if(runtimer == 0){
-                stateid = 0;
-                blocktimer = structs[0]->process->io_time;
+        // do the following for every process that has arrived and is not in a ready state
+        for(int i = 0; i < numberOfProcesses; i++){
+            current = structs[i];
+            
+            if(current -> terminationState != 1){
+                sprintf(cycleResults[current->process->pid], "%d:%s", current->process->pid, states[current -> processState]);
+                if(current -> processState == 0){
+                    current -> blockTimer = current -> blockTimer - 1;
+                    if(current -> blockTimer == 0){
+                        //set to ready state
+                        current -> processState = 1;
+                        current -> runTimer = current -> burstTime;
+                    }
+                }
+                else if(current -> processState == 2){
+                    current -> remainingTime = current ->remainingTime - 1;
+                    current -> runTimer = current -> runTimer - 1;
+                    if(current -> runTimer == 0){
+                        //set process to blocked stated
+                        current -> processState = 0;
+                        Dequeue(&head);
+                        current -> blockTimer = current -> process -> io_time;
+                    }
+                }
+                if(current -> remainingTime == 0){
+                    current -> terminationState = 1;
+                }
             }
         }
-        printf("%d %s\n", currentCycle, cycleResults[0]);
+        //print out all the analytics for waiting processes in a for loop
+        printf("%d %s %s\n", currentCycle, cycleResults[0], cycleResults[1]);
         currentCycle++;
     }
 
--- scheduler/list.h
+++ scheduler/list.h
@@ -11,12 +11,16 @@
 
 struct node {
     Process *process;
-    int processState;
-    int terminationState;
-    int arrivalState;
+    int processState; // 0 = blocked, 1 = ready, 2 = running
+    int terminationState; // 0 = not terminated, 1 = terminated
+    int arrivalState; // 0 = not arrived, 1 = arrived
+    int queuedState; // 0 = not in queue, 1 = in queue
+    int burstTime; // the number of cycles a process spends in a cpu burt
+    int remainingTime; // keeps track of how many cycles the process has run for
+    int runTimer; // keeps track of how many more cycles the process has run during a given cpu burts
+    int blockTimer; // keeps track of how many more cycles the process must be blocked for during a given i/o burst
     struct node *next;
 };
 
-// insert and delete operations.
 
 //note Process *process = struct process *process
